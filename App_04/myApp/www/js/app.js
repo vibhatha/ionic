e4550befbf694633115515b63570d73fcea5c2a2
vibhatha <vibhatha@gmail.com>
2015-07-15 22:15:17 -0700

    REST API Implemented

---
+++ App_04/myApp/www/js/app.js
@@ -0,0 +1,97 @@
+// Ionic Starter App
+//https://api.mongolab.com/api/1/databases/my-db/collections/my-coll?apiKey=qdhJK5uStPt3XnQOZ5WAeyjXp76L32g8
+//https://api.mongolab.com/api/1/databases/mean-development-3/articles?apiKey=qdhJK5uStPt3XnQOZ5WAeyjXp76L32g8
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers','nvd3','ngResource','ngRoute','mongolabResourceHttp']).constant('MONGOLAB_CONFIG',{API_KEY:'qdhJK5uStPt3XnQOZ5WAeyjXp76L32g8', DB_NAME:'mean-development-3'})
+.factory('Articles', function($mongolabResourceHttp) {
+	//
+    return $mongolabResourceHttp('articles');
+})
+
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/search.html"
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.graph', {
+      url: "/graph",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/graph.html",
+          controller: 'GraphCtrl'
+        }
+      }
+    })////
+   .state('app.articles', {
+      url: "/articles",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/articles.html",
+          controller: 'ArticlesCtrl'
+        }
+      }
+    })
+//////
+  .state('app.single', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html",
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
+
